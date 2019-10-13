3defa6cc4d99ebeac5fb17961164a5fbbcb06746
Frank Mathy <frankmathy@gmail.com>
2019-10-14 00:13:02 +0200

    Added firebase inserts

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import axios from '../firebase/FirebaseAccess'
+import Spinner from './Spinner'
 
 class NameForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { value: ''}
+        this.state = { 
+            value: '',
+            result: 'n/a',
+            loading: false
+        }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
@@ -13,11 +19,31 @@ class NameForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value)
+        /*alert('A name was submitted: ' + this.state.value)*/
+        this.setState({loading: true})
+        const entry = {
+            timestamp: new Date(),
+            name: this.state.value
+        }
+        axios.post('/nameform.json', entry)
+            .then(response => {
+                this.setState({loading: false})
+                this.setState({ result: response.statusText })
+                console.log(response)
+            })
+            .catch(response => {
+                this.setState({loading: false})
+                this.setState({ result: response.statusText })
+                console.log(response)
+            })
         event.preventDefault()
-    }
+}
 
     render() {
+        let resultElements = <p>Result: {this.state.result}</p>
+        if (this.state.loading) {
+            resultElements = <Spinner/>
+        }
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
@@ -25,7 +51,7 @@ class NameForm extends React.Component {
                     <input type="text" value={this.state.value} onChange={this.handleChange}/>
                 </label>&nbsp;
                 <input type="submit" value="Submit"/>
-                <p>Eingegebener Wert: {this.state.value}</p>
+                {resultElements}
             </form>
         )
     }
